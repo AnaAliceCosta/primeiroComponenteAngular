977327b8a182272eba9c9187673f6e879a898ee8
AnaAliceCosta <anaalice.cd@gmail.com>
2021-03-04 13:30:07 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cursos/cursos/cursos.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent implements OnInit {
+
+  nomePortal:string;
+  cursos: string[] =["java", "angular","react"]
+
+  constructor() { 
+    this.nomePortal =  'www.anaalicecosta.com.br'
+    for(let i=0;i<this.cursos.length;i++){
+      let curso = this.cursos[i]
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+}
